5480f58d9862eafe9909c1d087041b6cdd90f428
guilhermesif <guilhermesimfer@gmail.com>
2025-03-22 23:29:12 -0300

    feat: Implementation of animation in the login input box.

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -12,7 +12,13 @@ export default function AuthPage() {
           <div className="bg-black/70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
             <div className="flex flex-col gap-4">
-              <Input />
+              <Input
+                label="Email"
+         //       onChange={(e) => console.log(e.target.value)}
+                id="email"
+                type="email"
+                value=""
+              />
             </div>
           </div>
         </div>
