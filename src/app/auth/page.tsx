1ac7af8b7f6433378d2e4a67d1b74804dc01c514
guilhermesif <guilhermesimfer@gmail.com>
2025-03-27 21:38:17 -0300

    feat: add auth page email and name state

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 import Image from "next/image";
 import Input from "../../components/input";
+import { useState } from "react";
 
 export default function AuthPage() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black lg:bg-black/50 w-full h-full">
@@ -14,11 +18,17 @@ export default function AuthPage() {
             <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
             <div className="flex flex-col gap-4">
               <Input
+                label="Username"
+                onChange={(ev) => setName(ev.target.value)}
+                id="name"
+                value={name}
+              />
+              <Input
                 label="Email"
-                onChange={(e) => console.log(e.target.value)}
+                onChange={(ev) => setEmail(ev.target.value)}
                 id="email"
                 type="email"
-                value=""
+                value={email}
               />
             </div>
           </div>
