ee0e4de71119a5a944f285b1be5a78e29cf6f025
guilhermesif <guilhermesimfer@gmail.com>
2025-04-06 21:03:07 -0300

    feat: add signin and signup switch functionality

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 import Image from "next/image";
 import Input from "../../components/input";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export default function AuthPage() {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) => currentVariant === "login" ? "register" : "login");
+  }, []);
   
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -16,15 +22,19 @@ export default function AuthPage() {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black/70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign In" : "Register"}
+            </h2>
             <div className="flex flex-col gap-4">
-              <Input
-                label="Username"
-                onChange={(ev) => setName(ev.target.value)}
-                id="name"
-                value={name}
-              />
-              <Input
+              {variant === "register" && (
+                <Input
+                  label="Username"
+                  onChange={(ev) => setName(ev.target.value)}
+                  id="name"
+                  value={name}
+                />
+              )}
+                <Input
                 label="Email"
                 onChange={(ev) => setEmail(ev.target.value)}
                 id="email"
@@ -40,11 +50,11 @@ export default function AuthPage() {
               />
             </div>
             <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-              Login
+              {variant === "login" ? "Login" : "Sign Up"}
             </button>
             <p className=" text-neutral-500 mt-12">First time using Netflix?
-              <span className="text-white ml-1 hover:underline cursor-pointer">
-                Create account
+              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                Create an account
               </span>
             </p>
           </div>
