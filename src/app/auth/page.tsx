6cff02d67cc631d8d40a8d99c6df56874623796a
Guilherme Ferreira <guilhermesimfer@gmail.com>
2025-05-09 13:01:07 -0300

    feat: add registration functionality to AuthPage with API integration

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,65 +1,91 @@
-"use client";
-import Image from "next/image";
-import Input from "../../components/input";
-import { useCallback, useState } from "react";
+'use client';
+import Image from 'next/image';
+import Input from '../../components/input';
+import { useCallback, useState } from 'react';
+import axios from 'axios';
 
 export default function AuthPage() {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
+	const [email, setEmail] = useState('');
+	const [name, setName] = useState('');
+	const [password, setPassword] = useState('');
 
-  const [variant, setVariant] = useState<'login' | 'register'>("login");
+	const [variant, setVariant] = useState<'login' | 'register'>('login');
 
-  const toggleVariant = useCallback(() => {
-    setVariant((currentVariant) => currentVariant === "login" ? "register" : "login");
-  }, []);
-  
-  return (
-    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-      <div className="bg-black lg:bg-black/50 w-full h-full">
-        <nav className="px-2 py-0">
-          <Image src="/images/logo.png" alt="Logo" width={240} height={60} />
-        </nav>
-        <div className="flex justify-center">
-          <div className="bg-black/70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant === "login" ? "Sign In" : "Register"}
-            </h2>
-            <div className="flex flex-col gap-4">
-              {variant === "register" && (
-                <Input
-                  label="Username"
-                  onChange={(ev) => setName(ev.target.value)}
-                  id="name"
-                  value={name}
-                />
-              )}
-                <Input
-                label="Email"
-                onChange={(ev) => setEmail(ev.target.value)}
-                id="email"
-                type="email"
-                value={email}
-              />
-              <Input
-                label="Password"
-                onChange={(ev) => setPassword(ev.target.value)}
-                id="password"
-                type="password"
-                value={password}
-              />
-            </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-              {variant === "login" ? "Login" : "Sign Up"}
-            </button>
-            <span className=" text-neutral-500 mt-12"> {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
-              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
-                {variant === 'login' ? 'Create an account' : 'Login'}
-              </span>
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const toggleVariant = useCallback(() => {
+		setVariant(currentVariant =>
+			currentVariant === 'login' ? 'register' : 'login'
+		);
+	}, []);
+
+	const register = useCallback(async () => {
+		try {
+			await axios.post('/api/auth/register', {
+				email,
+				name,
+				password,
+			});
+			toggleVariant();
+		} catch (error) {
+			console.log(error);
+		}
+	}, [email, name, password, toggleVariant]);
+
+	return (
+		<div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+			<div className='bg-black lg:bg-black/50 w-full h-full'>
+				<nav className='px-2 py-0'>
+					<Image src='/images/logo.png' alt='Logo' width={240} height={60} />
+				</nav>
+				<div className='flex justify-center'>
+					<div className='bg-black/70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
+						<h2 className='text-white text-4xl mb-8 font-semibold'>
+							{variant === 'login' ? 'Sign In' : 'Register'}
+						</h2>
+						<div className='flex flex-col gap-4'>
+							{variant === 'register' && (
+								<Input
+									label='Username'
+									onChange={ev => setName(ev.target.value)}
+									id='name'
+									value={name}
+								/>
+							)}
+							<Input
+								label='Email'
+								onChange={ev => setEmail(ev.target.value)}
+								id='email'
+								type='email'
+								value={email}
+							/>
+							<Input
+								label='Password'
+								onChange={ev => setPassword(ev.target.value)}
+								id='password'
+								type='password'
+								value={password}
+							/>
+						</div>
+						<button
+							onClick={register}
+							className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'
+						>
+							{variant === 'login' ? 'Login' : 'Sign Up'}
+						</button>
+						<span className=' text-neutral-500 mt-12'>
+							{' '}
+							{variant === 'login'
+								? 'First time using Netflix?'
+								: 'Already have an account?'}
+							<span
+								onClick={toggleVariant}
+								className='text-white ml-1 hover:underline cursor-pointer'
+							>
+								{variant === 'login' ? 'Create an account' : 'Login'}
+							</span>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
