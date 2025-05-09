8dff0d286d46d02b7ab3e09cf7022a6b299781a4
Guilherme Ferreira <guilhermesimfer@gmail.com>
2025-05-09 12:55:05 -0300

    style: adjust quotes for consistency in the global.d.ts file

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,8 @@
-import  { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 declare global {
-  namespace globalThis {
-    // eslint-disable-next-line no-var
-    var prismadb: PrismaClient | undefined;
-  }
+	namespace globalThis {
+		// eslint-disable-next-line no-var
+		var prismadb: PrismaClient | undefined;
+	}
 }
